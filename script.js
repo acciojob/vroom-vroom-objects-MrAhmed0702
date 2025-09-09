35bf5d90a68ee647217d4d0bb07dfd696e443cf1
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-09-09 22:59:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-// Complete the js code
-function Car(make, model) {
-	this.make = make;
-	this.model = model;
-}
-	
-Car.prototype.getMakeModel = function() {
-	return `${this.make} ${this.model}`;
+// Complete the
+
+class Car {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  getMakeModel() {
+    return `${this.make} ${this.model}`;
+  }
 }
 
-function SportsCar(make, model, topSpeed) {
-	Car.call(this, make, model);
-	this.topSpeed = topSpeed;
-}
+class SportsCar extends Car {
+  constructor(make, model, topSpeed) {
+    super(make, model); // Call the parent constructor
+    this.topSpeed = topSpeed;
+  }
 
-SportsCar.prototype = Object.create(Car.prototype);
-SportsCar.prototype.constructor = SportsCar;
-
-SportsCar.prototype.getTopSpeed = function() {
-	return `${this.topSpeed}`;
-}
+  getTopSpeed() {
+    return this.topSpeed;
+  }
 
 // Do not change the code below
 window.Car = Car;
